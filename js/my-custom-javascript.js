b573f51466cc5a5bfb6b5cd07a4e3ebfb6dfdeec
Nicholas VanDyken <vandyken.nicholas@gmail.com>
2022-10-31 14:39:37 -0400

    added for exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,12 +1,20 @@
 "use strict";
 
 $(function() {
+ $(`h1`).click(function(){
+	 $(this).css(`background-color`, `yellow`);
+    });
+ 
+$(`p`).dblclick(function (){
+		$(this).css(`font-size`, `18px`)
+    });
 	
-	// alert(`the DOM has finished loading!`);
-	$(`h1`).css(`background-color`, `yellow`)
-	$(`li`).css(`font-size`, `20px`)
-	$(`h1, p, li`).css(`background-color`, `yellow`)
-	let text= $(`h1`).text();
-	alert(text);
-	
+$(`li`).hover(function(){
+	$(this).css(`color`, `red`);
+}, function (){
+	$(this).css(`color`, `black`);
 });
+
+
+});
+
